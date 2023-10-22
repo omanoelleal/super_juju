4be1ff73b4383f3284e5aae34c94646b9e1fcc92
Manoel Leal <manoel.leal17@gmail.com>
2023-10-22 17:23:27 +0100

    Update

--- js/script.js
+++ js/script.js
@@ -1,18 +1,33 @@
 const princess = document.querySelector('.princess');
 const suggar = document.querySelector('.suggar');
+const botaorestart = document.getElementById("restart");
+let score_val = document.querySelector('.score_val');
+let game_over_message = document.querySelector('.game_over_message');
+let score_title = document.querySelector('.score_title');
+let score_message = document.querySelector('.score_message');
+const audioPulo = document.getElementById("audioPulo");
+const audioGameOver = document.getElementById("audioGameOver");
 
 const jump = () => {
+    audioPulo.play();
     princess.classList.add('jump');
     setTimeout(() => {
         princess.classList.remove('jump');
     }, 500);
 }
+
+score_title.innerHTML = 'SCORE ';
+score_val.innerHTML = '0';
+
  
-const loop = setInterval(() => {
+contador = 0;
 
+const loop = setInterval(() => { 
 
+    botaorestart.style.display = "none";
 
-    const suggarPosition = suggar.offsetLeft;
+    let suggarPosition = suggar.offsetLeft;
+    const suggarPositionScore = +window.getComputedStyle(suggar).right.replace('px', '');
     const princessPosition = +window.getComputedStyle(princess).bottom.replace('px', '');
 
     if (suggarPosition <= 130 && suggarPosition > 0 && princessPosition < 80) {
@@ -27,8 +42,30 @@ const loop = setInterval(() => {
         princess.style.width = "130px"
         princess.style.marginLeft = "20px"
 
+        audioGameOver.play();
+
+        game_over_message.innerHTML = 'GAME OVER';
+        score_message.innerHTML = `YOUR SCORE ${score_val.innerHTML}`;
+        botaorestart.style.display = "block";
+        
         clearInterval(loop);
+        
     }
+    if (suggarPosition < 0 && princessPosition >= 0) {
+        contador++;
+        if (contador == 1) {
+            score_val.innerHTML ++;
+        }
+    }
+    else {
+        contador = 0;
+    }
+    
+})
+
+
+botaorestart.addEventListener("click", function () {
+    location.reload();
 })
 
 document.addEventListener("keydown", jump);
